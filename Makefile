f7f33d9951455bf7460aff954428821793f1cb0a
theverygaming <example@example.com>
2021-12-13 19:04:57 +0100

    added simple print file info

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CC = g++
 CFLAGS  = -Wall  
+LIBS = -lsndfile
 TARGET = idk
 
 all: $(TARGET)
  
 $(TARGET): $(TARGET).cpp
-	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).cpp
+	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).cpp $(LIBS)
  
 clean:
 	$(RM) $(TARGET)
--- idk.cpp
+++ idk.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <sndfile.h>
 
 int main() {
-    std::cout << "it works!";
+    //Read the WAV file and get some info on it, print that info
+    SNDFILE *infile;
+    SF_INFO fileinfo;
+    infile = sf_open("test.wav", SFM_READ, &fileinfo);
+    if (infile == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+    printf("File info:\nSamplerate: %d\nFrames/Samples: %lu\nChannels: %d\nFormat: %d\nSections: %d\nSeekable: %d\n", fileinfo.samplerate, fileinfo.frames, fileinfo.channels, fileinfo.format, fileinfo.sections, fileinfo.seekable);
+
+
     return 0;
 }
