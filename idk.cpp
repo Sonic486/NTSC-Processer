4082ff346527dea422ef35b81d0cb1db28897d20
theverygaming <clemibunge@gmail.com>
2021-12-14 00:36:44 +0100

    Some progress, using a comb filter now

--- idk.cpp
+++ idk.cpp
@@ -30,8 +30,44 @@ int main() {
     }
     free(InFileArray);
     
+    
+
+    unsigned int alignment = volk_get_alignment();
+    lv_32fc_t* outputComplex = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t)*fileinfo.frames, alignment);
+    // The oscillator rotates at f=0.1
+    float sinAngle = 2.0 * 3.14159265359 * 59960 / fileinfo.samplerate;
+    lv_32fc_t phase_increment = lv_cmake(std::cos(sinAngle), std::sin(sinAngle));
+    lv_32fc_t phase= lv_cmake(1.f, 0.0f); // start at 1 (0 rad phase)
+    volk_32fc_s32fc_x2_rotator_32fc(outputComplex, inputComplex, phase_increment, &phase, fileinfo.frames);
+    free(inputComplex);
 
     
+    
+    int k = 163;
+    float a = 0.9f;
+    for(long int i = k; i < fileinfo.frames - k; i++) {
+        float real = outputComplex[i].real() + (a * outputComplex[i - k].real());
+        float imag = outputComplex[i].imag() + (a * outputComplex[i - k].imag());
+        std::complex<float> thecomplex(real, imag);
+        outputComplex[i] = thecomplex;
+    }
+    
+
+
+    printf("converting back to real type\n");
+    float *outputReal = (float*) calloc(fileinfo.frames * fileinfo.channels, sizeof(float));
+    for(long int i = 0; i < fileinfo.frames; i++) {
+        outputReal[(2 * i)] = outputComplex[i].real();
+        outputReal[(2 * i)+1] = outputComplex[i].imag();
+    }
+    free(outputComplex);
+
+    printf("writing to file\n");
+    SNDFILE *outFile;
+    SF_INFO outFileInfo = fileinfo;
+    outFile = sf_open("out.wav", SFM_WRITE, &outFileInfo);
+    sf_writef_float(outFile, outputReal, fileinfo.frames);
+	sf_close(outFile);
 
     
     return 0;
