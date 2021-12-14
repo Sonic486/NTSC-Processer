1fd26678e4e54b59e6e35c5ca9f7e10e48c987f4
theverygaming <clemibunge@gmail.com>
2021-12-14 09:12:32 +0100

    Add DC spike removal and calculate K for comb filter

--- idk.cpp
+++ idk.cpp
@@ -8,7 +8,7 @@ int main() {
     //Read the WAV file and get some info on it, print that info
     SNDFILE *infile;
     SF_INFO fileinfo;
-    infile = sf_open("test.wav", SFM_READ, &fileinfo);
+    infile = sf_open("donno.wav", SFM_READ, &fileinfo);
     if (infile == NULL) {
         printf("Error opening file.\n");
         return 1;
@@ -24,13 +24,28 @@ int main() {
     sf_readf_float(infile, InFileArray, fileinfo.frames); //Should probably not do this, later read the file in shorter pieces since that way it will not use an infinite amount of memory
     std::complex<float> *inputComplex = (std::complex<float>*) calloc(fileinfo.frames, sizeof(std::complex<float>));
     for(long int i = 0; i < fileinfo.frames; i++) {
-        std::complex<float> thecomplex(InFileArray[(2 * i)], InFileArray[(2 * i)+1]); //i don't know if the first channel is real or imaginary, probably is real though
-        inputComplex[i] = thecomplex;
-        //Also there probably is a much be a better way to assign a complex number to an array?
+        inputComplex[i] = std::complex<float>(InFileArray[(2 * i)], InFileArray[(2 * i)+1]); //I knew there was a better way!
     }
     free(InFileArray);
     
-    
+    //Remove DC offset, or at least try to
+    int DCOffsetAverage = 300;
+    for(long int i = 0; i < fileinfo.frames; i+=DCOffsetAverage) {
+        float averageReal = 0;
+        float averageImag = 0;
+        for(int j = 0; j < DCOffsetAverage; j++) {
+            averageReal += inputComplex[i+j].real();
+        }
+        for(int j = 0; j < DCOffsetAverage; j++) {
+            averageImag += inputComplex[i+j].imag();
+        }
+        averageReal /= DCOffsetAverage;
+        averageImag /= DCOffsetAverage;
+        for(int j = 0; j < DCOffsetAverage; j++) {
+            inputComplex[i+j] -= std::complex<float>(averageReal, averageImag);
+        }
+    }
+
 
     unsigned int alignment = volk_get_alignment();
     lv_32fc_t* outputComplex = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t)*fileinfo.frames, alignment);
@@ -43,7 +58,8 @@ int main() {
 
     
     
-    int k = 163;
+    int k = std::round((float)fileinfo.frames / 15625);
+    printf("%d\n",k);
     float a = 0.9f;
     for(long int i = k; i < fileinfo.frames - k; i++) {
         float real = outputComplex[i].real() + (a * outputComplex[i - k].real());
